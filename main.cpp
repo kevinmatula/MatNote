97cbb5dc7d3998f88bbc2da6c4658f27bbaac5f8
kevinmatulaa <matulakevin91@gmail.com>
2025-09-27 20:39:56 -0400

    feat: window popup

--- main.cpp
+++ main.cpp
@@ -2,6 +2,12 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// This is a function that is called every time a user tries to resize window
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+const unsigned int SCREEN_WIDTH = 800;
+const unsigned int SCREEN_HEIGHT = 600;
+
 int main() {
   // Initialization (setting GLFW to version 3, and setting profile to
   // core_profile for some reason)
@@ -11,8 +17,8 @@ int main() {
   glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
   // Initializing Window
-  GLFWwindow *window =
-      glfwCreateWindow(800, 600, "Rendering Window", NULL, NULL);
+  GLFWwindow *window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT,
+                                        "Rendering Window", NULL, NULL);
 
   if (window == NULL) {
     std::cout << "Failed to Display Window." << std::endl;
@@ -22,5 +28,34 @@ int main() {
 
   glfwMakeContextCurrent(window);
 
+  // Want to make sure GLAD is initalized before calling any OpenGL function
+  // because GLAD manages function pointers
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    std::cout << "Failed to initialize GLAD" << std::endl;
+    return -1;
+  }
+
+  glViewport(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  // Here, we tell GLFW we want to call this function on every window resize
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  // This while loop is also called the render loop, controls rendering.
+  while (!glfwWindowShouldClose(window)) {
+    // Swaps the color buffer and show it as output to the screen
+    // I think this works by being a double buffer, as in something is always
+    // drawn to the screen to reduce flickering
+    glfwSwapBuffers(window);
+    // Checks if any events are triggered, and updates the window state
+    // by calling corresponding functions (that are registered via clalback
+    // methods)
+    glfwPollEvents();
+  }
+  // Delete and clean all of GLFW's resources!
+  glfwTerminate();
   return 0;
+}
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
 }
